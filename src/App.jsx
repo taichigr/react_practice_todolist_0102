d6cc4516a7669efd785919e778a02d884b9f373c
taichi <taichi.01gr14@gmail.com>
2022-01-02 13:07:55 +0900

    make incomplete and make complete

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useCallback, useState } from "react";
+
+function App() {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+
+  const onClickAdd = () => {
+    if(todoText === "") return;
+    const newTodoList = [...incompleteTodos, todoText]
+    setIncompleteTodos(newTodoList)
+    setTodoText("");
+  }
+
+  const onClickToCompleteList = (index) => {
+    const newIncompleteList = [...incompleteTodos]
+    newIncompleteList.splice(index, 1)
+    const newCompleteList = [...completeTodos, incompleteTodos[index]]
+    setIncompleteTodos(newIncompleteList)
+    setCompleteTodos(newCompleteList)
+  }
+
+  const onClickDelete = (index, todoList, setTodoList) => {
+    const newTodoList = [...todoList]
+    newTodoList.splice(index, 1)
+    setTodoList(newTodoList)
+  }
+
+
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="container">
+          <div className="inputArea">
+            <input type="text" value={todoText} placeholder="text" autoFocus onChange={(e) => setTodoText(e.target.value)} />
+            <button className="c-button" onClick={onClickAdd}>追加</button>
+          </div>
+          <div className="todoAreaWraper">
+            <h3>incomplete Todo</h3>
+            <div className="inompleteTodo">
+                {incompleteTodos.map((todo, index) => (
+                  <ul key={index}>
+                    <li>{todo}</li>
+                    <li>
+                      <button className="c-button" onClick={() => onClickToCompleteList(index)}>完了</button>
+                    </li>
+                    <li>
+                      <button className="c-button" onClick={() => onClickDelete(index, incompleteTodos, setIncompleteTodos)}>削除</button>
+                    </li>
+                  </ul>
+                ))}
+            </div>
+          </div>
+          <div className="todoAreaWraper">
+            <h3>complete Todo</h3>
+            <div className="completeTodo">
+            {completeTodos.map((todo, index) => (
+                  <ul key={index}>
+                    <li>{todo}</li>
+                    <li>
+                      <button className="c-button" onClick={() => onClickDelete(index, completeTodos, setCompleteTodos)}>削除</button>
+                    </li>
+                  </ul>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
